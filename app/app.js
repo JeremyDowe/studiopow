d112c61f86cf5c0635fe4d8523f03c40120d2cc4
JeremyDowe <jeremy.dowe@gmail.com>
2019-12-13 09:50:12 +1000

    good morning

--- app/app.js
+++ app/app.js
@@ -46,16 +46,18 @@ $(function(){
         // Math recipes
         const paddingScroll = (height / 3) * scrolling;
         const scroll = Math.sqrt(scrolling / height) + 1;
-        const axisScroll = this.Math.tan(2/this.Math.PI)/height;
-        const cosScroll = this.Math.cos(2/this.Math.PI)/height;
+        const axisScroll = this.Math.tan(2/this.Math.PI)/(height*0.3332);
+        const cosScroll = this.Math.cos(2/this.Math.PI)/(height*0.3332);
         const opacityScroll = Math.log(this.Math.PI)/this.Math.log(this.Math.cosh(scroll))-1;
         // scrolling behaviour logo / menu
         $('#logo').addClass('normal');
-        $('nav ul li').addClass('normal');        
+        $('nav ul li').addClass('normal');    
         if ( $('.page__part__two').height() !== this.undefined || $('.page__part__three').height() !== this.undefined ) {
+            const scrollPart2 = $('.page__part__two').position().top;   
+            const scrollPart3 = $('.page__part__two').position().top;
             if (
-                (scrolling > offset && scrolling > $('.page__part__two').position().top - offset) && 
-                (scrolling < $('.page__part__three').position().top - offset)
+                (scrolling > offset && scrolling > scrollPart2 - offset) && 
+                (scrolling < scrollPart3 - offset)
             ){
                 $('#logo').removeClass('invert');
                 $('nav ul li').removeClass('invert');
@@ -70,17 +72,26 @@ $(function(){
             // scrolling behaviour text / page
             if (
                 (scrolling > offset) && 
-                (scrolling < $('.page__part__two').position().top - offset)
+                (scrolling < scrollPart2 + offset)
             ){
                 $('.center').css('transform', 'translateY(' + Math.sqrt(paddingScroll*0.2) + 'px)');
-                $('.page__part__one').css('opacity', opacityScroll );
-                $('.center').css('opacity', opacityScroll );
+                if (scrolling > scrollPart2 && scrolling < scrollPart2 + offset){
+                    $('.page__part__one').css('opacity', '0' );
+                    $('.center').css('opacity', '0' );
+                } else {
+                    $('.page__part__one').css('opacity', opacityScroll );
+                    $('.center').css('opacity', opacityScroll );
+                }
                 $('.background__wrapper').css('transform', 'scale(' + scroll + ')');
+            }
+            if (scrolling < offset) {
+                $('.page__part__one').css('opacity', '1');
+                $('.center').css('opacity', '1' );
             }
             // scrolling to work section
             if (
                 (scrolling > offset) &&
-                (scrolling > $('.page__part__two').position().top - offset)
+                (scrolling > scrollPart2 - offset)
             )
             {
                 // animate in gsap
@@ -100,7 +111,7 @@ $(function(){
             // scrolling to work section
             if (
                 (scrolling > offset) &&
-                (scrolling > $('.page__part__three').position().top - offset)
+                (scrolling > scrollPart3 - offset)
             )
             {
                 // animate in gsap
@@ -119,15 +130,15 @@ $(function(){
             // work section scroll
             if (scrolling > lastScrollPos) {
                 // downscroll code
-                $('.work__piece').css('transform', 'translateY( ' + (cosScroll*scrolling) + 'px )');
+                $('.work__piece').css('transform', 'translateY( ' + scrolling/this.Math.log(cosScroll) + 'px )');
                 // title
-                $('.work__title').css('transform', 'translateY( ' + (axisScroll*scrolling) + 'px )');
+                $('.work__title').css('transform', 'translateY( ' + scrolling/this.Math.log2(axisScroll) + 'px )');
                 //console.log('down');
             } else {
                 // upscroll code
-                $('.work__piece').css('transform', 'translateY( ' + (axisScroll/scrolling) + 'px )');
+                $('.work__piece').css('transform', 'translateY( ' + scrolling/this.Math.log(axisScroll) + 'px )');
                 // title
-                $('.work__title').css('transform', 'translateY( ' + (cosScroll/scrolling) + 'px )');
+                $('.work__title').css('transform', 'translateY( ' + scrolling/this.Math.log2(cosScroll) + 'px )');
                 //console.log('up');
             }
             lastScrollPos = scrolling;
