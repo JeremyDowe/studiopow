4d6342e0f231081b20f828c83ed826dffc5058a3
JeremyDowe <jeremy.dowe@gmail.com>
2019-12-27 09:36:29 +1100

    updates

--- dist/contact.b15d9a0d.js
+++ dist/contact.b15d9a0d.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../assets/img/cobro-ViopfTBaHSs-unsplash.jpg":[["cobro-ViopfTBaHSs-unsplash.84ebed74.jpg","assets/img/cobro-ViopfTBaHSs-unsplash.jpg"],"assets/img/cobro-ViopfTBaHSs-unsplash.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../assets/img/contactpage.jpg":[["contactpage.257eaeed.jpg","assets/img/contactpage.jpg"],"assets/img/contactpage.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53480" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61652" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
